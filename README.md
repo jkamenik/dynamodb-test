5c5db837a335dad227454432aea4ffa467d13248
John Kamenik <jkamenik@gmail.com>
2014-04-03 10:48:25 -0400

    Added more functions to make iterating the store easy

--- README.md
+++ README.md
@@ -18,14 +18,33 @@ Or install it yourself as:
 
 ## Usage
 
-Create a .env containing
+Create a .env containing your amazon keys
 
     AMAZON_KEY=your_amazon_key
 		AMAZON_SECRET=your_amazon_secret
 
-Run the `dynamodb` command
 
-    $ dynamodb
+Run the `dynamodb commands` command to get a list of commands.
+
+    $ dynamodb commands
+
+### Useful commands
+
+1. tables - will show you all the tables you have
+1. table <name> - will show you the full info for one table
+1. items <table name> - iterates on all items in a given table (do not run on large tables)
+1. add <table name> <key=value> [<key1=value>] - adds an item to a table
+1. rm <table name> <primary key> <range key>
+
+
+## Seeded Setup
+
+If you don't already have a set of tables you can create a seeded set of tables like this
+
+    $ dynamodb create_seed_table test-table
+		...wait...
+		$ dynamodb seed test-table
+
 
 ## Contributing
 
--- lib/dynamodb/test/cli.rb
+++ lib/dynamodb/test/cli.rb
@@ -24,19 +24,45 @@ module Dynamodb
       end
       
       def commands(*args)
-        puts "commands - Lists commands"
-        puts "tables   - Shows all tables"
+        puts "commands                        - Lists commands"
+        puts "tables                          - Shows all tables"
+        puts "table <name>                    - Shows full info on one table"
+        puts "rmtable <name>                  - Removes a table"
+        puts "items <table name>              - Shows all items in a given table (do not run on large tables)"
+        puts "count <table name>              - Shows the number of items in table"
+        puts "add <table name> <key=value>... - Add a new item"
+        puts "rm <table name> <primary key>   - Removes an item"
+        puts "create_seed_table <table name>  - Creates a new table"
+        puts "seed <table name>               - Seeds a table with values"
       end
       
       def tables(*args)
         db.tables.each do |table|
           puts "#{table.name}: #{table.status}"
-          puts "\tRead:  #{table.read_capacity_units}"
-          puts "\tWrite: #{table.write_capacity_units}"
-          puts "\tItems: #{table.item_count}"
-          puts "\tKey:   #{table.hash_key.name} - #{table.hash_key.type}"
-          puts "\tRange: #{table.range_key.name} - #{table.range_key.type}"
         end
+      end
+      
+      def table(*args)
+        table = get_table(args.shift)
+        
+        puts "#{table.name}: #{table.status}"
+        puts "\tRead:  #{table.read_capacity_units} units"
+        puts "\tWrite: #{table.write_capacity_units} units"
+        puts "\tItems: #{table.item_count}"
+        puts "\tKey:   #{table.hash_key.name} - #{table.hash_key.type}"
+        puts "\tRange: #{table.range_key.name} - #{table.range_key.type}" unless table.range_key.nil?
+      end
+      
+      def rmtable(*args)
+        name = args.shift
+        puts "Deleting table #{name.inspect}"
+        table = get_table(name)
+        table.delete
+        while table.status == :deleting
+          print '.'
+          sleep 1
+        end
+        puts 'done'
       end
       
       def items(*args)
@@ -50,14 +76,57 @@ module Dynamodb
         end
       end
       
+      def create_seed_table(*args)
+        name   = args.shift
+        reads  = args.shift || 1
+        writes = args.shift || 1
+
+        puts "Creating table #{name.inspect}, this could take awhile"
+        table = db.tables.create(name, Integer(reads), Integer(writes),
+          hash_key: {url: :string}
+        )
+        while table.status == :creating
+          print '.'
+          sleep 1
+        end
+        puts 'done'
+      end
+      
       def seed(*args)
         table = get_table(args.shift)
+
+        pages      = %W(index contact_us about_us)
+        microsites = %W(ehc wildbird test)
         
-        table.items.create(
-          account_id: 'test',  # primary key
-          page:       'foo',   # range
-          content:    '<html></html>'
-        )
+        microsites.each do |site|
+          pages.each do |page|
+            table.items.create(
+              url: "#{site}/#{page}",
+              content: "<html></html>"
+            )
+          end
+        end
+        puts "Items: #{table.item_count}"
+      end
+      
+      def add(*args)
+        table      = args.shift
+        attributes = {}
+        
+        args.each do |item|
+          key,value = item.split('=')
+          attributes[key] = value
+        end
+        
+        get_table(table).items.create(attributes).attributes.each do |key,value|
+          puts "#{key}: #{value.inspect}"
+        end
+      end
+      
+      def rm(*args)
+        table = args.shift
+        
+        get_table(table).items[*args].delete
       end
       
       private
